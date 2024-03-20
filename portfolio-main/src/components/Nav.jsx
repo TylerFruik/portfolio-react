d48eb470501fa22d0ad5a3d46855d9b87eaa0478
TylerFruik <tylerfruik@gmail.com>
2024-03-20 16:59:04 -0500

    fixed about page error

--- portfolio-main/src/components/Nav.jsx
+++ portfolio-main/src/components/Nav.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <>
-      <h1>Nav</h1>
-      <Link to='/'>Tyler Fruik</Link>
-      <nav className=''>
+    <header>
+      <h1>Tyler Fruik</h1>
+      <nav className='navbar'>
+        <Link to='/'>About Me</Link>
         <Link to='/portfolio'>Portfolio</Link>
         <Link to='/resume'>Resume</Link>
         <Link to='/contact'>Contact Me</Link>
       </nav>
-    </>
+      </header>
   )
 }
 
