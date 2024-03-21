d8c7ea8bbce9d89be0a6d3ccaa5a820364b5597f
TylerFruik <tylerfruik@gmail.com>
2024-03-21 13:48:02 -0500

    Moved Contact Me page links to footer

--- portfolio-main/src/components/Foot.jsx
+++ portfolio-main/src/components/Foot.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Foot = () => {
   return (
     <footer>
-      <p>Last updated: 3/20/2024</p>
+        <div class="content" id="contact-me-content">
+          <nav>
+              <a href="https://github.com/TylerFruik" target="_blank">GitHub</a>
+              <Link to='/contact'>Email</Link>
+              <a href="tel:+17154390345"><span>(715) 439-0345</span></a>
+              <a href="https://discord.gg/kEWsKpq9" target="_blank">Discord</a>
+              <a href="https://linkedin.com/in/tyler-fruik/" target="_blank">LinkedIn</a>
+          </nav>
+        </div>
     </footer>
   )
 }
--- portfolio-main/src/pages/Contact/Contact.jsx
+++ portfolio-main/src/pages/Contact/Contact.jsx
@@ -7,14 +7,8 @@ const Contact = () => {
         <div class="title">
           <h3>Contact</h3>
         </div>
-        <div class="content" id="contact-me-content">
-          <nav>
-              <a href="https://github.com/TylerFruik" target="_blank">GitHub</a>
-              <a href="#"><span>Email</span></a>
-              <a href="#"><span>(715) 439-0345</span></a>
-              <a href="https://discord.gg/kEWsKpq9" target="_blank">Discord</a>
-              <a href="https://linkedin.com/in/tyler-fruik/" target="_blank">LinkedIn</a>
-          </nav>
+        <div className='form'>
+
         </div>
       </div>
     </div>
