b5891dc8641be5af2a239dd859c2cdc8eb7d5b4a
TylerFruik <tylerfruik@gmail.com>
2026-08-28 09:19:53 -0500

    Fix Netlify SPA 404 on direct/refresh nav; update About Me accuracy (wife, typos); remove dead debug code from Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// * JQuery testing
-import { useEffect } from 'react';
-import $ from 'jquery';
-
 const Nav = () => {
-
-  useEffect(() => {
-    // jQuery code to execute on component mount
-    $('#date').css('background-color', 'red');
-    
-    return () => {
-      // jQuery code to execute on component unmount (clean-up)
-      $('#date').css('background-color', 'initial');
-    };
-  }, []); // Empty dependency array to run only once on mount
-
   return (
     <header>
       <div class="wave">
@@ -37,9 +22,6 @@ const Nav = () => {
       <Link to='/'>
         <h1>Tyler Fruik</h1>
       </Link>
-      {/* <div id='date'>
-        Testing
-      </div> */}
       <nav className='navbar'>
         <Link to='/'>About Me</Link>
         <Link to='/portfolio'>Portfolio</Link>
